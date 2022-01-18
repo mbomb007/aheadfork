c836883934070add7d7da3b1b9c25067f288de5f
mbomb007 <22807401+mbomb007@users.noreply.github.com>
2022-01-18 15:20:14 -0600

    If there's only one branch, just use it.

--- aheadfork.py
+++ aheadfork.py
@@ -21,9 +21,10 @@ def get_forks():
     session = requests.Session()
     forks = []
 
-    # auth
+    # Authentication
     auth_url = "https://api.github.com"
-    session.headers.update({'Authorization': 'token ' + GITHUB_API_TOKEN})  # should carry this header for all request
+    # Should carry this header for all requests
+    session.headers.update({'Authorization': 'token ' + GITHUB_API_TOKEN})
     resp = session.get(auth_url)
     if resp.status_code != 200:
         resp_json = resp.json()
@@ -37,10 +38,10 @@ def get_forks():
             time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(resp.headers.get("X-RateLimit-Reset"))))
         ))
 
-    # set header
+    # Set header
     session.headers.update({'Accept': 'application/vnd.github.v3+json'})
 
-    # get repo info
+    # Get repo info
     repo_url = "https://api.github.com/repos/%s" % REPO
     resp = session.get(repo_url)
     if resp.status_code != 200:
@@ -48,40 +49,46 @@ def get_forks():
         return []
     forks_count = json.loads(resp.text).get("forks")
             
-    # determine branches of repo likely to be the primary branch
     branches_url = "https://api.github.com/repos/%s/branches"
     branches = session.get(branches_url % REPO, headers={'Accept': 'application/vnd.github.v3+json'}).json()
-    primary_branch_options = []
-    for branch in branches:
-        branch_name = branch.get("name")
-        if branch_name == "master" or branch_name == "main" or branch.get("protected"):
-            primary_branch_options.append(branch_name)
     
-    # ask user to choose branch if cannot infer primary branch
-    if len(primary_branch_options) == 0:
-        print('Could not determine primary branch for repo.')
-        branch_names = str([branch.get("name") for branch in branches])
-        primary_branch = input('Please enter the name of the primary branch. Branch list: \n%s\n' % branch_names)
-        if primary_branch not in branch_names:
-            print('Invalid branch name.')
-            exit(1)
-    elif len(primary_branch_options) > 1:
-        print('Could not determine primary branch for repo. %n options found.')
-        branch_names = str(primary_branch_options)
-        primary_branch = input('Please enter the name of the primary branch. Branch list: \n%s\n' % branch_names)
-        if primary_branch not in branch_names:
-            # see if a valid branch that wasn't a likely candidate was entered
-            for branch in branches:
-                if branch.get('name') == primary_branch:
-                    break;
-            else:
+    if len(branches) == 1:
+        # There is only one branch, so use it instead of looking for something non-existent.
+        primary_branch = branches[0]
+    else:
+        # Determine branches of repo likely to be the primary branch
+        primary_branch_options = []
+        for branch in branches:
+            branch_name = branch.get("name")
+            if branch_name == "master" or branch_name == "main" or branch.get("protected"):
+                primary_branch_options.append(branch_name)
+
+        # Ask user to choose branch if cannot infer primary branch
+        if len(primary_branch_options) == 0:
+            print('Could not determine primary branch for repo.')
+            branch_names = str([branch.get("name") for branch in branches])
+            primary_branch = input('Please enter the name of the primary branch. Branch list: \n%s\n' % branch_names)
+            if primary_branch not in branch_names:
                 print('Invalid branch name.')
                 exit(1)
-    else:
-         primary_branch = primary_branch_options[0]
+        elif len(primary_branch_options) > 1:
+            print('Could not determine primary branch for repo. %n options found.')
+            branch_names = str(primary_branch_options)
+            primary_branch = input('Please enter the name of the primary branch. Branch list: \n%s\n' % branch_names)
+            if primary_branch not in branch_names:
+                # See if a valid branch that wasn't a likely candidate was entered
+                for branch in branches:
+                    if branch.get('name') == primary_branch:
+                        break;
+                else:
+                    print('Invalid branch name.')
+                    exit(1)
+        else:
+             primary_branch = primary_branch_options[0]
 
-    # get forks
-    page_count = ceil(forks_count / 30)  # 30 items each page
+    # Get forks
+    # 30 items per page
+    page_count = ceil(forks_count / 30)  
     for page in range(1, page_count + 1):
         params = {"page": page}
         forks_url = "https://api.github.com/repos/%s/forks" % REPO
@@ -101,7 +108,7 @@ def get_forks():
                 "forks_count": repo.get("forks_count")
             })
 
-    # compare (ONLY compare primary branch)
+    # Compare (ONLY compare primary branch)
     compare_branches = "%s...%s" % (primary_branch, primary_branch)
     compare_url = "https://api.github.com/repos/%s/compare/%s:" + compare_branches
     for fork in forks:
@@ -137,7 +144,7 @@ if __name__ == "__main__":
     try:
         forks = get_forks()
         if forks:
-            # only show fork that is ahead of original repo. sorted by ahead_by and pushed_at
+            # Only show forks that are ahead of the original repo. Sorted by ahead_by and pushed_at
             forks = list(filter(lambda i: i.get("ahead_by"), forks))
             forks = sorted(forks, key=lambda i: (i.get("ahead_by"), i.get("pushed_at")), reverse=True)
 
